af01b562b73c212ea69bc7eddb27985f68bc5129
André <84719490+gggandre@users.noreply.github.com>
2022-08-16 10:05:07 -0500

    change spiral

--- GoldenSpiral/spiral.py
+++ GoldenSpiral/spiral.py
@@ -1,27 +1,23 @@
 #code made in class with teacher ariel
 #A01753176 Gilberto André García Gaytán
 from math import sqrt
-from turtle import circle, fd, pencolor, pensize,rt, done, circle, pencolor, pensize, speed
+from turtle import fd, rt, done, circle, pencolor, pensize, speed
+
 
 PHI = 1 / ((sqrt(5) - 1) / 2)
 
-#def square(n: int):
-  #  for _ in range(4):
-   #     fd(n)
-     #   rt(90)
-     
-def spiral(times: int) -> None:
-    lenght = 5
-    for _ in range(times):
-        circle(lenght, 90)
-        lenght *= PHI
 
-if __name__ == '__main__': 
-   # for _ in range(36):
-      #  square(100)      
-      #  rt(10)
-    pensize(10)
-    speed(10)
+def spiral(times: int) -> None:
+    length = 5.0
+    for _ in range(times):
+        circle(length, 90)
+        length *= PHI
+
+
+if __name__ == '__main__':
+    print(f'phi = {PHI}')
+    pensize(5)
     pencolor('medium purple')
-    spiral(10)
+    speed('fastest')
+    spiral(9)
     done()
