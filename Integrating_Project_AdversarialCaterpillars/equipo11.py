d57c7ef7462fa4b1c383f341794060555aa00205
André <84719490+gggandre@users.noreply.github.com>
2022-11-08 09:53:47 -0600

    names

---
+++ Integrating_Project_AdversarialCaterpillars/equipo11.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------
+# Project: Adversarial Caterpillars
+# Permutations and combinations with repetition.
+#
+# Date: 28-Nov-2022
+# Authors:
+#           A01745336 Diego Alejandro Balderas Tlahuitzo
+#           A01753176 Gilberto André García Gaytán
+# ----------------------------------------------------------
