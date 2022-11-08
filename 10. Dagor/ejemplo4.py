cd1c496247b934175d065cd10cfdf6413db0ea59
André <84719490+gggandre@users.noreply.github.com>
2022-11-08 10:27:20 -0600

    ejemplo4

--- 10. Dagor/ejemplo4.py
+++ 10. Dagor/ejemplo4.py
@@ -1,7 +1,7 @@
 from dagor import JuegoOrugas, JugadorOrugasInteractivo
 from dagor import JugadorOrugasAleatorio
 
-jugador1 = JugadorOrugasInteractivo('Cosa 1')
+jugador1 = JugadorOrugasAleatorio('Cosa 1')
 jugador2 = JugadorOrugasAleatorio('Machine')
-juego = JuegoOrugas(jugador1, jugador2, 4, 4)
-juego.inicia()
+juego = JuegoOrugas(jugador1, jugador2, 10, 10)
+juego.inicia(veces=100)
